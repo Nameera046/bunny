5c63852b1b4d975e0e08d53bcec65d5f3b8cbe8a
Nameera046 <2315030@nec.edu.in>
2026-07-01 17:00:34 +0530

    style changes done

--- practice2/frontend-test-submission/src/App.jsx
+++ practice2/frontend-test-submission/src/App.jsx
@@ -1,27 +1,27 @@
 import "./App.css";
 function App(){
   return (
-      <div style={{display:"flex",flexDirection:"column", justifyContent:"center",alignItems:"center",padding:"20px"}}>
+      <div className="body">
           <div>
             <h1>URL Shortner</h1>
           </div>
-          <div style={{display:"flex",justifyContent:"center",alignItems:"center",width:"80%",textAlign:"center",padding:"10px"}}>
+          <div className="para-section">
             <p>Welcome to the URL Shortener Dashboard! This application allows you to  convert long and complex URLs into short, easy-to-share links.</p>
           </div>
-          <div style={{display:"flex",flexDirection:"column",justifyContent:"center",alignItems:"center",width:"40%",border:"2px solid black",borderRadius:"10px",padding:"20px"}}>
-            <div style={{display:"flex",flexDirection:"column",padding:"5px",width:"90%",marginTop:"10px"}}>
-              <label style={{fontWeight:"bold"}}>URL</label>
-              <input type="text" placeholder="Enter the Url" style={{height:"25px",borderRadius:"4px",marginBottom:"5px",marginTop:"5px"}} />   {/*className=field-box*/}
+          <div className="section">
+            <div className="section1">
+              <label className="label">URL</label>
+              <input type="text" placeholder="Enter the Url" className="field-box" />   {/*className=field-box*/}
             </div>
-            <div style={{display:"flex",flexDirection:"column",padding:"5px",width:"90%"}}>
-              <label style={{fontWeight:"bold"}}>Validity (in Minutes)</label>
-              <input type="text" placeholder="Enter the validity" style={{height:"25px",borderRadius:"4px",marginBottom:"5px",marginTop:"5px",}}/>{/*className=field-box*/}
+            <div className="section1">
+              <label className="label">Validity (in Minutes)</label>
+              <input type="text" placeholder="Enter the validity" className="field-box"/>{/*className=field-box*/}
             </div>
-            <div style={{display:"flex",flexDirection:"column",padding:"5px",width:"90%",marginBottom:"10px"}}>
-              <label style={{fontWeight:"bold"}}>ShortCode</label>
-              <input type="text" placeholder="Enter the shortcode" style={{height:"25px",borderRadius:"4px",marginTop:"5px",marginBottom:"10px"}}/>{/*className=field-box*/}
+            <div className="section1">
+              <label className="label">ShortCode</label>
+              <input type="text" placeholder="Enter the shortcode" className="field-box"/>{/*className=field-box*/}
             </div>
-            <button style={{backgroundColor:"#7692ce", borderRadius:"5px",display:"flex",justifyContent: "center",alignItems:"center",width:"80px",height:"30px",fontWeight:"bold"}}>Submit</button>
+            <button className="button">Submit</button>
           </div>
       </div>
 
